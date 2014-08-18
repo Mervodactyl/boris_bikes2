16c5629fb2e792918c4c34245aea062081004c6d
Mervodactyl <merve.silk@gmail.com>
2014-08-18 16:19:22 +0100

    added extentions to full tests, using lambda and if methods

--- lib/docking_station.rb
+++ lib/docking_station.rb
@@ -1,7 +1,10 @@
 class Docking_station
 
-	def initialize
+	DEFAULT_CAPACITY = 20
+
+	def initialize(options = {})
 		@bikes ||= []
+		@capacity ||= DEFAULT_CAPACITY
 	end
 
 	def bikes
@@ -9,7 +12,19 @@ class Docking_station
 	end
 
 	def dock(bike)
+		raise "Station is full" if full?
 		bikes << bike
 	end
 
+	def release_bike
+		bikes.pop
+	end
+
+	def available_bikes
+		bikes.delete_if {|bikes| bikes.broken?}
+	end
+
+	def full?
+		bikes.count == @capacity
+	end
 end
--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -3,13 +3,36 @@ require 'docking_station'
 describe Docking_station do
 
 let(:bike) { double :bike, {:broken? => false}}
-let(:broken_bike) { double :bike, {:broken? => true}}
+let(:broken_bike) { double :bike, {:broken? => true} }
+let(:station) { Docking_station.new(:capacity => 20) }
 
 	it "should be able to dock a bike" do
-		station = Docking_station.new
 		expect(station.bikes).to eq []
 		station.dock(bike)
 		expect(station.bikes).to eq [bike]
+	end
+
+	it "should be able to release a bike" do
+		station.dock(bike)
+		station.release_bike
+		expect(station.bikes).to eq []
+	end
+
+	it "should provide a list of available bikes" do
+		station.dock(bike)
+		station.dock(broken_bike)
+		expect(station.available_bikes).to eq [bike]
+	end
+
+	it "should know if it's reached maximum capacity" do
+		expect(station).not_to be_full
+		20.times { station.dock(bike) }
+		expect(station).to be_full
+	end
+
+	it "should not accept a bike if it's full" do
+		20.times { station.dock(bike) }
+		expect(lambda {station.dock(bike)}).to raise_error(RuntimeError)
 	end
 
 end
