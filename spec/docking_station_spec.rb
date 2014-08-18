cc252da876088afdd000669a846b63999bbc33e0
Mervodactyl <merve.silk@gmail.com>
2014-08-18 15:53:30 +0100

    changed original test, fixed test using shovel and or methods

--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -2,11 +2,14 @@ require 'docking_station'
 
 describe Docking_station do
 
-	let(:bike) { double :bike, {broken? => false}}
-	let(:broken_bike) { double :bike, {broken? => true}}
+let(:bike) { double :bike, {:broken? => false}}
+let(:broken_bike) { double :bike, {:broken? => true}}
 
-	it "should be able to release a bike" do
-		expect(Docking_station.new.release_bike).to eq true
+	it "should be able to dock a bike" do
+		station = Docking_station.new
+		expect(station.bikes).to eq []
+		station.dock(bike)
+		expect(station.bikes).to eq [bike]
 	end
 
 end
